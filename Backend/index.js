eb771c1884c608c0c1d1d12cfc05207832f4c4f2
RoustyTousty <80759208+RoustyTousty@users.noreply.github.com>
2024-04-29 09:24:39 +0300

    hi

--- Backend/index.js
+++ Backend/index.js
@@ -11,46 +11,6 @@ app.use('/squere', require('./routes/squere'));
 app.use('/contact', require('./routes/contact'));
 app.use('/posts', require('./routes/posts'));
 
-// app.get('/posts', (req, res) => {
-//     try {
-//         knex.select('*').from('blog_posts')
-//         .then((rows) => {
-//             res.send(rows);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         })
-//     } catch(err) {
-//         res.status(500).send({error: err.message})
-//     }
-// })
-
-// app.get('/posts/:id', (req, res) => {
-//     try {
-//         knex.select('*').from('blog_posts').where('id', req.params.id)
-
-//         .then((rows) => {
-//             res.send(rows);
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//         })
-        
-//     } catch(err) {
-//         res.status(500).send({error: err.message})
-//     }
-// })
-
-// app.post('/posts', (req, res) => {
-//     try {
-//         knex('blog_posts').insert({title: req.body.title, body: req.body.body}).then(
-//             res.status(201).send({title: req.body.title, body: req.body.body})
-//         )
-//     } catch (err) {
-//         res.status(500).send({error: err.message})
-//     }
-// })
-
 const port = 5000;
 app.listen(port, () => {
     console.log("Server is runnng!");
